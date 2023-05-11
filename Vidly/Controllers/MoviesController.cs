a8411387372ef7de2c26a239b0d3d1bd522f524a
ARK <arklearnersedge@gmail.com>
2023-05-11 08:45:24 +0530

    Vidly project entire data

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //Get: Movies/Random
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Sherk!" }; 
+            return View(movie);
+            
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Content("id =" + id);
+        }
+    }
+}
